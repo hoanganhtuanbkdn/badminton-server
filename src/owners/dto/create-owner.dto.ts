ec7e0f038cdf84af20460cb8299e050c150c6822
Tuan PHA <tuanpha.it@gmail.com>
2024-09-01 14:52:10 +0700

    feat: update owner

--- src/owners/dto/create-owner.dto.ts
+++ src/owners/dto/create-owner.dto.ts
@@ -1,20 +1,52 @@
-import { IsString, IsEmail, IsNotEmpty } from 'class-validator';
+import { IsString, IsEmail, IsNotEmpty, Length, Matches, IsOptional } from 'class-validator';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { PartialType } from '@nestjs/mapped-types';
 
 export class CreateOwnerDto {
-  @ApiProperty({ description: 'Tên của chủ (Owner)' })
+  @ApiProperty({ description: 'Tên của chủ (Owner)', example: 'John Doe' })
   @IsString()
   @IsNotEmpty()
   name: string;
 
-  @ApiProperty({ description: 'Email của chủ' })
+  @ApiPropertyOptional({ description: 'Slug của chủ', example: 'john-doe' })
+  @IsString()
+  @IsOptional()
+  slug?: string;
+
+  @ApiProperty({ description: 'Email của chủ', example: 'owner@example.com' })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
-  @ApiProperty({ description: 'Số điện thoại của chủ' })
+  @ApiProperty({ description: 'Số điện thoại của chủ', example: '+1234567890' })
   @IsString()
   @IsNotEmpty()
   phoneNumber: string;
+
+  @ApiProperty({
+    description: 'Tên tài khoản ngân hàng. Nhập tiếng Việt không dấu, viết hoa, tối thiểu 5 ký tự, tối đa 50 kí tự, không chứa các ký tự đặc biệt.',
+    example: 'JOHN DOE',
+  })
+  @IsString()
+  @Length(5, 50)
+  @Matches(/^[A-Z0-9 ]+$/, { message: 'Account name must contain only uppercase letters, numbers, and spaces.' })
+  @IsOptional()
+  accountName?: string;
+
+  @ApiPropertyOptional({
+    description: 'Tên ngân hàng',
+    example: 'Bank of America',
+  })
+  @IsString()
+  @IsOptional()
+  bankName?: string;
+
+  @ApiProperty({
+    description: 'Số tài khoản ngân hàng thụ hưởng. Chỉ nhập số, tối thiểu 6 ký tự, tối đa 19 kí tự',
+    example: '1234567890',
+  })
+  @IsString()
+  @Length(6, 19)
+  @Matches(/^\d+$/, { message: 'Số tài khoản ngân hàng thụ hưởng. Chỉ nhập số, tối thiểu 6 ký tự, tối đa 19 kí tự.' })
+  @IsOptional()
+  accountNo?: string;
 }
